6323bbeb752832b1393b7ea71f8600611f6fa8c1
DashingDilwala <ff12133.08@gmail.com>
2025-01-20 00:07:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Bot(Client):
         await super().start()
         me = await self.get_me()
         self.username = '@' + me.username          
-        print('Bot Started Powered By @Anonymousdeveloper098)
+        print('Bot Started Powered By @Anonymousdeveloper098')
 
     async def stop(self, *args):
         await super().stop()
